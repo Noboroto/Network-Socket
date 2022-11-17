0ff5c14e9e3504026830c78e3b73a1eefe061730
David0403 <48942146+Noboroto@users.noreply.github.com>
2022-11-17 09:46:41 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Network_Socket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
