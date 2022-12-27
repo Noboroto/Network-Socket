a866f1e2482fa2d0c59d27757c990d76ca9aab01
David0403 <48942146+Noboroto@users.noreply.github.com>
2022-12-27 18:41:02 +0700

    Done Report

--- ExceptionHandler/HelpException.cs
+++ ExceptionHandler/HelpException.cs
@@ -6,7 +6,7 @@ namespace NetworkSocket.ExceptionHandler
     {
         private const string _message = "Option flags:\n"
                                        + "\t\t-h, --help \t\t\t\t: show help message!\n"
-                                       + "\t\t-p, --port \t<port number>\t\t\t: port for server. Defaut 8080\n"
+                                       + "\t\t-p, --port \t<port number>\t\t: port for server. Defaut 8080\n"
                                        + "\t\t-f, --folder \t<source path>\t\t: source code folder\n";
         public HelpException() : base (_message)
         { 
